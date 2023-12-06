1db21ab5aefe04d3a7334fbd7e93acd9b6aa6c6c
Alma Garcia <almarg98@gmail.com>
2023-12-05 22:40:50 -0800

    Changed repo script

--- files/arctic_monkeys_script.R
+++ files/arctic_monkeys_script.R
@@ -6,8 +6,8 @@ Sys.setenv(SPOTIFY_CLIENT_SECRET = 'xxxxxxxxxxxxxxxxxxxxx')
 
 access_token <- get_spotify_access_token()
 
-arctic_monkeys <- get_artist_audio_features('arctic monkeys')
+AM <- get_artist_audio_features('arctic monkeys')
 
-arctic_monkeys %>%
-	select(track_name, track_number, album_name, album_release_date, danceability, energy, loudness, speechiness, duration_ms) %>%
+AM %>%
+	select(track_name, track_number, album_name, album_release_date, album_release_year, danceability, energy, loudness, speechiness, duration_ms, tempo, valence) %>%
 	write.csv(file = "arctic_monkeys.csv")
